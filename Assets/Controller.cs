dbe1424bb186afdd6b40e2ae048898b29c4a8b87
kazimieras_Griskaitis <griskaitis.kazimieras@gmail.com>
2020-08-31 08:13:34 +0100

    initial unity fast style transfer commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Unity.Barracuda;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public List <NNModel> modelAsset;
+    private List <Model> m_RuntimeModel;
+    private int styleNumber;
+    List<IWorker> worker;
+    public RawImage rawImage;
+    int height;
+    int width;
+    // Start is called before the first frame update
+
+    public RenderTexture basicView;
+    private RenderTexture outputTexture;
+    void Start()
+    {
+        worker = new List<IWorker>();
+        m_RuntimeModel = new List<Model>();
+        height = Screen.height/4;
+        width = Screen.width/4;
+        outputTexture = new RenderTexture(width, height, basicView.depth);
+        basicView.height = height/2;
+        basicView.width = width/2;
+        styleNumber = -1;
+
+        foreach(NNModel model in modelAsset)
+        {
+            Model temp = ModelLoader.Load(model);
+            m_RuntimeModel.Add(temp);
+            worker.Add(WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, temp));
+        }
+    }
+    int i = -1;
+    Coroutine cor;
+    // Update is called once per frame
+    void Update()
+    {
+        bool switchStyle = Input.GetKeyDown(KeyCode.LeftShift);
+        if (switchStyle)
+        {
+            if(cor != null)
+            {
+                StopCoroutine(cor);
+            }
+            styleNumber++;
+
+            if (styleNumber >= modelAsset.Count)
+            {
+                rawImage.enabled = false;
+                styleNumber = -1;
+            }
+            else
+            {
+                if (styleNumber == 0)
+                {
+                    rawImage.enabled = true;
+                    
+                }
+                cor = StartCoroutine(Classify());
+            }
+        }
+
+
+        if (styleNumber > -1)
+        {
+
+            //Tensor tensor = new Tensor(basicView, 3);
+            //worker[styleNumber].Execute(tensor);
+            
+            
+            //var output = worker[styleNumber].PeekOutput();
+            //output.ToReadOnlyArray();
+            //output.ToRenderTexture(outputTexture, 0, 0, 1 / 255f, 0, null);
+            //rawImage.texture = outputTexture;
+            //output.Dispose();
+        }
+    }
+
+    private IEnumerator Classify()
+    {
+        while (true)
+        {
+            Tensor tensor = new Tensor(basicView, 3);
+            var enumerator = this.worker[styleNumber].StartManualSchedule(tensor);
+            int skip_layers = 0;
+            while (enumerator.MoveNext())
+            {
+                skip_layers++;
+                if (skip_layers >= 20)
+                {
+                    skip_layers = 0;
+                    yield return null;
+                }
+            };
+
+            // this.worker.Execute(inputs);
+            // Execute() scheduled async job on GPU, waiting till completion
+            // yield return new WaitForSeconds(0.5f);
+
+            var output = worker[styleNumber].PeekOutput();
+            output.ToReadOnlyArray();
+            output.ToRenderTexture(outputTexture, 0, 0, 1 / 255f, 0, null);
+            rawImage.texture = outputTexture;
+            output.Dispose();
+            yield return null;
+        }
+
+    }
+}
---
+++ Tutorial Defaults/Controller.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Unity.Barracuda;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public NNModel modelAsset;
+    private Model m_RuntimeModel;
+    IWorker worker;
+    public RawImage canvas;
+    public Material material;
+    int height;
+    int width;
+    // Start is called before the first frame update
+
+    public RenderTexture basicView;
+    void Start()
+    {
+        height = Screen.height;
+        width = Screen.width;
+        
+        m_RuntimeModel = ModelLoader.Load(modelAsset);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, m_RuntimeModel);
+    }
+
+    // Update is called once per frame
+    [Obsolete]
+    void Update()
+    {
+        GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y, 0);
+        float vertical = Input.GetAxis("Horizontal");
+        bool jump = Input.GetButtonDown("Jump");
+        if(vertical != 0)
+            GetComponent<Rigidbody>().velocity = new Vector3(vertical*10, GetComponent<Rigidbody>().velocity.y, 0);
+        if (jump)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 0));
+        }
+        if (!jump)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Tensor tensor = new Tensor(basicView, 3);
+            Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
+            texture.Resize(256, 256);
+            Color32[] color = texture.GetPixels32();
+            width = height = 256;
+            Tensor tensoro = TransformInput(color, width, height);
+            worker.Execute(tensoro);
+            var output = worker.PeekOutput();
+            material.mainTexture = BarracudaTextureUtils.TensorToRenderTexture(output);
+            canvas.texture = material.mainTexture;
+            tensor.Dispose();
+            output.Dispose();
+            stopwatch.Stop();
+            UnityEngine.Debug.Log(stopwatch.ElapsedMilliseconds);
+        }
+    }
+
+        [SerializeField] private int _screenshotTextureW = 1280, _screenshotTextureH = 720;
+
+        public Texture2D ScreenshotTexture { get; private set; }
+
+        private void Awake()
+        {
+            ScreenshotTexture = new Texture2D(_screenshotTextureW, _screenshotTextureH, TextureFormat.RGB24, false);
+        }
+    
+
+
+    public const int IMAGE_SIZE = 224;
+    private const int IMAGE_MEAN = 127;
+    private const float IMAGE_STD = 127.5f;
+    private Tensor TransformInput(Color32[] pic, int v1, int v2)
+    {
+        float[] floatValues = new float[width * height * 3];
+
+        for (int i = 0; i < pic.Length; ++i)
+        {
+            var color = pic[i];
+
+            floatValues[i * 3 + 0] = (color.r - IMAGE_MEAN) / IMAGE_STD;
+            floatValues[i * 3 + 1] = (color.g - IMAGE_MEAN) / IMAGE_STD;
+            floatValues[i * 3 + 2] = (color.b - IMAGE_MEAN) / IMAGE_STD;
+        }
+
+        return new Tensor(1, height, width, 3, floatValues);
+    
+    }
+}
